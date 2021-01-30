329b688980a5eecc47c7be0aaab467c2a6238ff6
Jonathan Pesce <jonathanpesce@hotmail.com>
2021-01-30 15:17:52 -0500

    Optimize the webcam to be slightly more efficient

--- VirtualWebcam/VirtualWebcam.py
+++ VirtualWebcam/VirtualWebcam.py
@@ -15,9 +15,9 @@ import win32api
 # Constants
 IMG_W = 640
 IMG_H = 480
-ERROR_THRESHOLD = 40
-SLEEPING_THRESHOLD = 10
-USER_THRESHOLD = 10
+ERROR_THRESHOLD = 4
+SLEEPING_THRESHOLD = 2
+USER_THRESHOLD = 2
 BLUR_TEXT = 'Personal Stuff Happening'
 SLEEPING_TEXT = 'BLURED BECAUSE SLEEPING'
 WM_APPCOMMAND = 0x319
@@ -32,7 +32,6 @@ class VirtualWebcam():
         self.open_eyes_cascade = cv.CascadeClassifier(str(pathlib.Path(__file__).resolve().parent)  + './Haarcascades/haarcascade_eye.xml')
         self.right_eyes_cascade = cv.CascadeClassifier(str(pathlib.Path(__file__).resolve().parent)  + './Haarcascades/haarcascade_righteye_2splits.xml')
         self.left_eyes_cascade = cv.CascadeClassifier(str(pathlib.Path(__file__).resolve().parent)  + './Haarcascades/haarcascade_lefteye_2splits.xml')
-        # breakpoint()
         self.terminate = True
         self.noFaceDetected = 0
         self.isSleeping = False
@@ -44,6 +43,8 @@ class VirtualWebcam():
         self.blurredImg = None
         self.face_recognizer = cv.face.LBPHFaceRecognizer_create()
         self.face_recognizer.read(str(pathlib.Path(__file__).resolve().parent)  + '/Data/JonathanPesce_Model.yml')
+        self.counter = 0
+    
     
     def start(self):
         self.terminate = False
@@ -56,18 +57,15 @@ class VirtualWebcam():
         if (video_feed.isOpened() == False):
             return "ERROR - Could not connect to webcam!!!"
 
-        counter = 0
-
         with pyvirtualcam.Camera(width=IMG_W, height=IMG_H, fps=15) as cam:
             print("Running")
             while True:
-                if counter == 30:
-                    self.checkSleep = (os.environ.get('BOOM_CHECK_SLEEPING') == '1')
-                    self.errImg = (os.environ.get('BOOM_CUSTOM_ERROR_IMG_PATH'))
-                    counter = 0
-
+                # if counter == 30:
+                #     self.checkSleep = (os.environ.get('BOOM_CHECK_SLEEPING') == '1')
+                #     self.errImg = (os.environ.get('BOOM_CUSTOM_ERROR_IMG_PATH'))
+                #     counter = 0
+                
                 frame = self.processFrame(video_feed)
-                # cv.imshow('Title', frame) 
                 
                 # Send to virtual cam
                 cam.send(frame)
@@ -77,15 +75,30 @@ class VirtualWebcam():
                     
                 if cv.waitKey(1) == 27:
                     break
+            
+            cv.waitKey(0)
+            video_feed.release()
+
+     
+            
+    def startPython(self):
+        self.terminate = False
+        self.noFaceDetected = 0
         
+        # Use OpenCV to grab the webcam video feed
+        video_feed = cv.VideoCapture(0)
         
+        # Check if the webcam can be opened
+        if (video_feed.isOpened() == False):
+            return "ERROR - Could not connect to webcam!!!"
+
+        while True:
+            frame = self.processFrame(video_feed)
+            cv.imshow('Title', frame) 
+            cv.waitKey(1)
         
         cv.waitKey(0)
         video_feed.release()
-        cv.destroyAllWindows()
-     
-            
-
 
 
     """
@@ -94,48 +107,56 @@ class VirtualWebcam():
     
     Return Frame to send to webcam feed
     """
-    def processFrame(self, video_feed):
+    def processFrame(self, video_feed, isPython=False):
         # Read the frame
         isTrue, frame = video_feed.read()
         
-        # Convert the frame to grayscale
-        frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        
-        # Get the faces that are detected in the frame
-        faces_rect = self.face_cascade.detectMultiScale(frame_gray, scaleFactor=1.2, minNeighbors=10)
-        
-        # Check if the person detected is sleeping
-        sleeping = self.checkSleep and self.checkForSleep(faces_rect, frame_gray)
-        
-        # Check if it is the current user
-        currentUser = self.testIfUser(faces_rect, frame_gray)
-        
-        # Check if no faces were detected, if so then increase no face detected counter
-        self.noFaceDetected = (0, self.noFaceDetected + 1)[len(faces_rect) < 1 or sleeping or currentUser]
-        
-        # Check if face is not detected, if so send blocking frame
-        if (self.noFaceDetected > ERROR_THRESHOLD):
-            frame = self.getBlockFrame(frame, sleeping, currentUser)
+        if (self.counter == 10):
+            self.counter = 0
+            
+            # Convert the frame to grayscale
+            frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            
+            # Get the faces that are detected in the frame
+            faces_rect = self.face_cascade.detectMultiScale(frame_gray, scaleFactor=1.2, minNeighbors=10)
+            
+            # Check if the person detected is sleeping
+            sleeping = self.checkSleep and self.checkForSleep(faces_rect, frame_gray)
+            
+            # Check if it is the current user
+            currentUser = self.testIfUser(faces_rect, frame_gray)
+            
+            # Check if no faces were detected, if so then increase no face detected counter
+            self.noFaceDetected = (0, self.noFaceDetected + 1)[len(faces_rect) < 1 or sleeping or currentUser]
+            
+            # Check if face is not detected, if so send blocking frame
+            if (self.noFaceDetected > ERROR_THRESHOLD):
+                frame = self.getBlockFrame(frame, sleeping, currentUser)
+            elif (self.blurredImg is not None):
+                win32api.SendMessage(-1, WM_APPCOMMAND, 0x30292, APPCOMMAND_MIC_MAX * 0x10000)
+                self.blurredImg = None
+                
         else:
-            win32api.SendMessage(-1, WM_APPCOMMAND, 0x30292, APPCOMMAND_MIC_MAX * 0x10000)
-            self.blurredImg = None
-        
-        
-        
-        # Draw the rectangles for debugging purposes
-        for (x,y,w,h) in faces_rect:
-            cv.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), thickness=1)
+            self.counter += 1
+            
+            if (self.blurredImg is not None):
+                frame = self.blurredImg
+            
+            
+            # # Draw the rectangles for debugging purposes
+            # for (x,y,w,h) in faces_rect:
+            #     cv.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), thickness=1)
         
             
-        
-        # convert to RGBA
-        out_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        out_frame_rgba = np.zeros((IMG_H, IMG_W, 4), np.uint8)
-        out_frame_rgba[:, :, :3] = out_frame
-        out_frame_rgba[:, :, 3] = 255
-        cv.flip(out_frame_rgba, -1)
-        # out_frame_rgba = frame    
-        
+        if (isPython):
+            out_frame_rgba = frame
+        else:
+            # convert to RGBA
+            out_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+            out_frame_rgba = np.zeros((IMG_H, IMG_W, 4), np.uint8)
+            out_frame_rgba[:, :, :3] = out_frame
+            out_frame_rgba[:, :, 3] = 255
+            cv.flip(out_frame_rgba, -1)
         
         return out_frame_rgba
         
@@ -202,9 +223,6 @@ class VirtualWebcam():
     
     
     def getBlockFrame(self, frame, sleeping, currentUser):
-        if (self.errImg is not None):
-            return self.errImg
-        
         if (self.blurredImg is None):
             win32api.SendMessage(-1, WM_APPCOMMAND, 0x30292, APPCOMMAND_MIC_MIN * 0x10000)
             
@@ -230,4 +248,5 @@ class VirtualWebcam():
 if(__name__ == str('__main__')):    
     # t = VirtualWebcam(errImgPath='ErrorImage.png', checkSleep=True)
     t = VirtualWebcam(checkSleep=True)
+    #t.startPython()
     t.start()
